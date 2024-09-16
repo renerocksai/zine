170567737efc5333b38886cfe83211e011267b24
Rene Schallner <rene@zml.ai>
2024-09-16 16:45:30 +0200

    github-ids: buffer-overrun protection for superlong titles

--- src/render/githubids.zig
+++ src/render/githubids.zig
@@ -4,9 +4,13 @@ pub fn printGithubId(title: []const u8, w: anytype) !void {
     const allowed_chars = "abcdefghijklmnopqrstuvwxyz0123456789- ";
     // since we need to strip leading and trailing hyphens, we cannot do it
     // on-the-fly
-    var buffer: [256]u8 = undefined;
+    const buf_size = 256;
+    var buffer: [buf_size]u8 = undefined;
     const lower = std.ascii.lowerString(&buffer, title);
     for (lower, 0..) |c, i| {
+        // let's not over-run our buffer
+        if (i >= buf_size) break;
+
         if (std.mem.indexOfScalar(u8, allowed_chars, c)) |_| {
             if (c == ' ') {
                 buffer[i] = '-';
